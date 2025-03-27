e7c9202178c8714dcd93ecf6fcf0ebfc33be5a86
Thomas Martin <116772338+ColdTerror@users.noreply.github.com>
2025-03-27 15:31:09 -0400

    Added main menu that was worked on in class

---
+++ Cancer Combat/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Scene Names")]
+    public string startSceneName = "Game"; 
+    public string optionsSceneName = "Options"; 
+    public string creditsSceneName = "Credits"; 
+
+    public void StartGame()
+    {
+        Debug.Log("Starting Game...");
+        SceneManager.LoadScene(startSceneName);
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Opening Options Scene...");
+        SceneManager.LoadScene(optionsSceneName);
+    }
+
+    public void OpenCredits()
+    {
+        Debug.Log("Opening Credits Scene...");
+        SceneManager.LoadScene(creditsSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
